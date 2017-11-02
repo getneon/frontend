f08cd580836418310133c504105075b6d57065b0
Cameron Baughn <cam.baughn@gmail.com>
2017-11-02 14:55:11 -0700

    Add topics and docs to redux

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -9,10 +9,38 @@ function user(state = {}, action) {
   }
 }
 
+function currentTopic(state = '', action) {
+  switch (action.type) {
+    case 'SET_CURRENT_TOPIC':
+      return action.topic;
+    default:
+      return state;
+  }
+}
+
+function topics(state = [], action) {
+  switch (action.type) {
+    case 'SET_TOPICS':
+      return action.topics;
+    default:
+      return state;
+  }
+}
+
+function docs(state = [], action) {
+  switch (action.type) {
+    case 'SET_DOCS':
+      return action.docs;
+    default:
+      return state;
+  }
+}
 
 
 const neonApp = combineReducers({
   user,
+  topics,
+  docs,
 });
 
-module.exports = { user, neonApp };
+module.exports = { user, currentTopic, topics, docs, neonApp };
